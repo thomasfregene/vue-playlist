8c89e3b5b99f454912ef3cda73f4e9b0a2bc12c1
thomasfregene <fregzythomas@gmail.com>
2021-02-25 09:22:53 +0100

    input binding using v-model

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div>
-    <keep-alive>
-      <component v-bind:is="component"></component>
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show form one</button>
-    <button v-on:click="component = 'form-two'">Show form two</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import formOne from "./components/formOne.vue";
-import formTwo from "./components/formTwo.vue";
-
+import addBlog from './components/addBlog.vue'
 export default {
-  components: {
-    "form-one": formOne,
-    "form-two": formTwo
+  components:{
+    'add-blog': addBlog
   },
   data() {
     return {
-      component: "form-two"
     };
   },
   methods: {}
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
