096eb5641262658556eaae0f19c4c46b29057af4
thomasfregene <fregzythomas@gmail.com>
2021-02-25 20:27:19 +0100

    data binding for select input

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -19,6 +19,10 @@
         <label for="">Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories" />
       </div>
+      <label for="">Authors:</label>
+      <select name="" id="" v-model="blog.author">
+          <option v-for="author in authors">{{author}}</option>
+      </select>
       <div id="preview">
         <h3>Preview Blog</h3>
         <p>Blog title: {{ blog.title }}</p>
@@ -28,6 +32,7 @@
         <ul>
             <li v-for="category in blog.categories">{{category}}</li>
         </ul>
+        <p>Authors: {{blog.author}}</p>
       </div>
     </div>
   </div>
@@ -40,8 +45,10 @@ export default {
       blog: {
         title: "",
         content: "",
-        categories: []
-      }
+        categories: [],
+        author: ""
+      },
+      authors: ['The Net Ninja', 'The Angular Avengers', 'The Vue Vindicator']
     };
   },
   methods: {}
