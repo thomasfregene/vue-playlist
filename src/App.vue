c8299da6255577942565ae8bf57ec486740f6865
thomasfregene <fregzythomas@gmail.com>
2021-02-25 07:34:05 +0100

    using slots for editable template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <form-helper>
-      <h2 slot="title">I am the slot title</h2>
-      <p slot="text">I am the paragraph text for the slot</p>
+      <div slot="form-header">
+        <h3>This is the form title</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required>
+        <input type="password" placeholder="password" required>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
@@ -16,6 +25,7 @@ export default {
 },
   data () {
     return{
+      title: 'I am a dynamic slot title'
     }
   },
   methods:{
--- src/components/formHelper.vue
+++ src/components/formHelper.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
-      <slot name="title"></slot>
-      <h1>I am the form helper</h1>
-      <slot name="text"></slot>
+      <h1>Please fill out our form...</h1>
+      <form action="">
+          <div id="form-header">
+              <slot name="form-header"></slot>
+          </div>
+          <div id="form-fields">
+              <slot name="form-fields"></slot>
+          </div>
+          <div id="form-controls">
+              <slot name="form-controls"></slot>
+          </div>
+          <div id="useful-links">
+              <ul>
+                  <li><a href="#">Link 1</a></li>
+                  <li><a href="#">Link 2</a></li>
+                  <li><a href="#">Link 3</a></li>
+                  <li><a href="#">Link 4</a></li>
+              </ul>
+          </div>
+      </form>
   </div>
 </template>
 
@@ -17,4 +34,29 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+h1{
+    text-align: center;
+}
+form{
+    width: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+}
+#useful-links ul{
+    padding: 0;
+}
+#useful-links li{
+    display: inline-block;
+    margin-top: 10px;
+}
+form > div{
+    padding: 20px;
+    background: #eee;
+    margin: 20px 0;
+}
+#form-header{
+    background: #ddd;
+    border: 1px solid #bbb;
+}
+</style>
