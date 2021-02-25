1cd9554e3ec77745aea5c15cdd1aad1b5989a772
thomasfregene <fregzythomas@gmail.com>
2021-02-25 08:44:14 +0100

    keeping component alive

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div>
-    <component v-bind:is="component"></component>
-    <button v-on:click="component='form-one'">Show form one</button>
-    <button v-on:click="component='form-two'">Show form two</button>
+    <keep-alive>
+      <component v-bind:is="component"></component>
+    </keep-alive>
+    <button v-on:click="component = 'form-one'">Show form one</button>
+    <button v-on:click="component = 'form-two'">Show form two</button>
   </div>
 </template>
 
 <script>
-import formOne from './components/formOne.vue'
-import formTwo from './components/formTwo.vue'
+import formOne from "./components/formOne.vue";
+import formTwo from "./components/formTwo.vue";
 
 export default {
-  components:{
-  'form-one': formOne,
-  'form-two': formTwo
-},
-  data () {
-    return{
-      component: 'form-two'
-    }
+  components: {
+    "form-one": formOne,
+    "form-two": formTwo
   },
-  methods:{
-  }
-}
+  data() {
+    return {
+      component: "form-two"
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style scoped></style>
