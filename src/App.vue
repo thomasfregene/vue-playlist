13a40f8a0a49c875d638ffc3e6c317c259e9536b
thomasfregene <fregzythomas@gmail.com>
2021-02-25 07:05:28 +0100

    using slots for different elements

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,24 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-
-    <app-ninja v-bind:ninjas="ninjas"></app-ninja>
-
-    <app-footer v-bind:title="title"></app-footer>
+    <form-helper>
+      <h2 slot="title">I am the slot title</h2>
+      <p slot="text">I am the paragraph text for the slot</p>
+    </form-helper>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Ninjas from './components/Ninjas.vue'
+import formHelper from './components/formHelper.vue'
+
 export default {
   components:{
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-ninja': Ninjas
-  },
+  'form-helper': formHelper
+},
   data () {
     return{
-        ninjas:[
-            {name: 'Ryu', speciality: 'Vue Components', show: false},
-            {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-            {name: 'Hitoshi', speciality: 'Click Events', show: false},
-            {name: 'Tango', speciality: 'Conditionals', show: false},
-            {name: 'Kami', speciality: 'Webpack', show: false},
-            {name: 'Koshi', speciality: 'Data Diggin', show: false},
-        ],
-        title: "Vue Ninja"
     }
-    
   },
   methods:{
-    updateTitle:function(updatedTitle){
-      this.title = updatedTitle;
-    }
   }
 }
 </script>
