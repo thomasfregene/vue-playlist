be9f6a24060bc8cdeff21e5272dba7f93dd5e6c7
thomasfregene <fregzythomas@gmail.com>
2021-02-25 20:15:23 +0100

    data binding for checkboxes

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-      <div id="add-blog">
-          <h2>Add a new blog Post</h2>
-          <form action="">
-              <label for="">Blog Title:</label>
-              <input type="text" v-model.lazy="blog.title" required>
-              <label for="">Blog Content</label>
-              <textarea name="" id="" cols="30" rows="10" v-model.lazy="blog.content"></textarea>
-          </form>
-          <div id="preview">
-              <h3>Preview Blog</h3>
-              <p>Blog title: {{blog.title}}</p>
-              <p>Blog content:</p>
-              <p>{{blog.content}}</p>
-          </div>
+    <div id="add-blog">
+      <h2>Add a new blog Post</h2>
+      <form action="">
+        <label for="">Blog Title:</label>
+        <input type="text" v-model.lazy="blog.title" required />
+        <label for="">Blog Content</label>
+        <textarea v-model.lazy="blog.content"></textarea>
+      </form>
+
+      <div id="checkboxes">
+        <label for="">Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories" />
+        <label for="">Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories" />
+        <label for="">Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories" />
+        <label for="">Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories" />
       </div>
+      <div id="preview">
+        <h3>Preview Blog</h3>
+        <p>Blog title: {{ blog.title }}</p>
+        <p>Blog content:</p>
+        <p>{{ blog.content }}</p>
+        <p>Blog categories:</p>
+        <ul>
+            <li v-for="category in blog.categories">{{category}}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,10 +37,11 @@
 export default {
   data() {
     return {
-        blog:{
-            title: "",
-            content: ""
-        }
+      blog: {
+        title: "",
+        content: "",
+        categories: []
+      }
     };
   },
   methods: {}
@@ -33,28 +49,36 @@ export default {
 </script>
 
 <style scoped>
-#add-blog *{
-    box-sizing: border-box;
+#add-blog * {
+  box-sizing: border-box;
 }
-#add-blog{
-    margin: 20px auto;
-    max-width: 500px;
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
 }
-label{
-    display: block;
-    margin: 20px 0 10px;
+label {
+  display: block;
+  margin: 20px 0 10px;
 }
-input[type="text"], textarea{
-    display: block;
-    width: 100%;
-    padding: 8px;
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
 }
-#preview{
-    padding: 10px 20px;
-    border: 1px dotted #ccc;
-    margin: 30px 0;
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
 }
-h3{
-    margin-top: 10px;
+h3 {
+  margin-top: 10px;
+}
+#checkboxes input {
+  display: inline-block;
+  margin-right: 10px;
+}
+#checkboxes label {
+  display: inline-block;
 }
 </style>
