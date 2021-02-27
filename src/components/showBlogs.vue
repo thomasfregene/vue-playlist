4e1a16e290d241fb99208e496099f76afd33eef4
thomasfregene <fregzythomas@gmail.com>
2021-02-28 00:09:52 +0100

    creating and using custom directives

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,10 +1,10 @@
 <template>
-  <div id="show-blogs">
-      <h1>All Blog Articles</h1>
-      <div v-for="blog in blogs" class="single-blog">
-          <h2>{{blog.title}}</h2>
-          <article>{{blog.body}}</article>
-      </div>
+  <div v-theme="'narrow'" id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2 v-rainbow>{{ blog.title }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
   </div>
 </template>
 
@@ -12,27 +12,29 @@
 export default {
   data() {
     return {
-        blogs:[]
+      blogs: []
     };
   },
   methods: {},
-  created(){
-      this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-          this.blogs = data.body.slice(0, 10);
-      })
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        this.blogs = data.body.slice(0, 10);
+      });
   }
 };
 </script>
 
 <style scoped>
-#show-blogs{
-    max-width: 800  px;
-    margin: 0 auto;
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
 }
-.single-blog{
-    padding: 20px;
-    margin: 20px 0;
-    box-sizing: border-box;
-    background: #eee;
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+//custom directives
+Vue.directive('rainbow', {
+  bind(el,binding, vnode){
+    el.style.color = "#" + Math.random().toString().slice(2,8)
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = '1200px';
+    }else if(binding.value == 'narrow'){
+      el.style.maxWidth = '560px'
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
