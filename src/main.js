9c00325930ba88602eb332abfb5549f86243884f
thomasfregene <fregzythomas@gmail.com>
2021-02-28 00:14:35 +0100

    using argument with a custom directives

--- src/main.js
+++ src/main.js
@@ -18,6 +18,10 @@ Vue.directive('theme', {
     }else if(binding.value == 'narrow'){
       el.style.maxWidth = '560px'
     }
+    if(binding.arg == 'column'){
+      el.style.background = "#ddd",
+      el.style.padding = "20px"
+    }
   }
 })
 
