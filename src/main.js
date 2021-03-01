ec1d4416112d87903a3df5d1b8aa0418afe6d42b
thomasfregene <fregzythomas@gmail.com>
2021-03-01 03:59:23 +0100

    setting up and using routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
+
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+})
 
 //filters
 Vue.filter('snippet', function(value){
@@ -11,5 +19,6 @@ Vue.filter('snippet', function(value){
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import showBlogs from './components/showBlogs.vue'
+import addBlog from './components/addBlog.vue'
+export default[
+    {path: '/', component: showBlogs},
+    {path: '/add', component: addBlog}
+]
